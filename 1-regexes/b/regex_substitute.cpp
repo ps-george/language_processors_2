5704cd83f2e78989c6a8e39b05d9b5dda248ea05
George Punter <punter.s.g@gmail.com>
2017-01-18 12:46:11 +0000

    Seems to be working. Should write testbench for this.

--- 1-regexes/b/regex_substitute.cpp
+++ 1-regexes/b/regex_substitute.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
-
+#include <string>
 #include <regex>
 
 int main(int argc, char *argv[])
+
 {
-    // TODO: read the regex
-    // TODO: read the substitution string
-    
+    // Read regex and subtituion string
+    std::regex rgx;
+    std::string sbs;
+
+    std::cout << argc - 1 << " arguments provided." <<std::endl;
+    if (argc==1){
+      std::cerr << "No regex or subsitution string provided." << std::endl;
+      return 1;
+    }
+    if (argc>1 && argc!=3){
+      rgx = argv[1];
+      std::cerr << "No subtitution string provided." << std::endl;
+      // return 1;
+    }
+    if (argc==3){
+      rgx = argv[1];
+      sbs = argv[2];
+    }
+    else {
+      std::cerr << "Too many arguments." << std::endl;
+      return 1;
+    }
+
     // TODO: for each input line
-    //   TODO: Apply the regex
-    //   TODO: print the new line out
-    
+    std::string tmp;
+    while( std::getline(std::cin, tmp) ){
+      //   Apply the regex and print out the new string
+      std::cout << std::regex_replace(tmp,rgx,sbs,std::regex_constants::format_sed) << std::endl;
+    }
     return 0;
 }
