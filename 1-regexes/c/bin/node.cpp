4176211d2edfd27748518160908692f0c1f6bc13
George Punter <gsp14@eews304-027.ee.ic.ac.uk>
2017-01-19 13:56:00 +0000

    fast commit

--- 1-regexes/c/bin/node.cpp
+++ 1-regexes/c/bin/node.cpp
@@ -1,34 +1,3 @@
 #include "node.hpp"
 
-Node::Node() {
-  end = false;
-  i = 0;
-}
 
-Node::Node(char c, nodePtr n) {
-  end = false;
-  i = 0;
-  set_arrow(c, n);
-}
-
-// Set one of the arrows from this node to another node
-void Node::set_arrow(char c, nodePtr n) {
-  arrows[i] = std::pair<char, nodePtr>(c, n);
-  i++;
-}
-
-// Test whether the given character matches any of the arrow chars
-// returns the number of arrows it has matched
-bool Node::test_match(char c) {
-  unsigned int count = 0;
-  for (const auto &itPair : arrows) {
-    if (itPair.first == c) {
-      count++;
-    }
-  }
-  return 0;
-}
-
-bool Node::is_empty(){
-  return i;
-}
--- 1-regexes/c/bin/node.hpp
+++ 1-regexes/c/bin/node.hpp
@@ -1,24 +1,104 @@
 #ifndef NODE_HPP_ 
 #define NODE_HPP_
 
+#include <list>
 #include <vector>
+#include <stack>
+#include <iostream>
 
-// Node is a NFA state with a number of arrows exiting.
-typedef class Node* nodePtr;
+using namespace std;
+
+typedef class Node *nodePtr;
+
+class Arrow {
+public:
+	Arrow(nodePtr n1, char c1, bool e) : n(n1),c(c1),epsilon(e){};
+  bool match(const char &c1) const;
+  nodePtr n;
+  char c;
+  bool epsilon;
+};
+
 class Node {
 public:
-  Node();
-  Node(char c, nodePtr n);
-  void set_arrow(char c, nodePtr n);
-  bool is_empty();
-  bool test_match(char c);
-  bool matched() const{ return end;}
-  
-private:
-  std::vector<std::pair<char, nodePtr>> arrows;
-  unsigned int i;
-  bool end;
+	Node();
+	Node(bool final);
+	void create_arrow(nodePtr n, char c, bool e);
+	bool is_blank() const;
+	std::vector<Arrow> arrows;
+	unsigned int size;
+	bool end;
 };
+
+Node::Node(){
+	end = false;
+	size = 0;
+}
+
+Node::Node(bool final){
+	end = final;
+	size = 0;
+}
+
+void Node::create_arrow(nodePtr n, char c, bool e){
+	arrows.push_back(Arrow(n,c,e));
+	size++;
+}
+
+// A fragment consists of a start state and a list of end states
+class Frag {
+public:
+	Frag::Frag(char c);
+	Frag create_frag(char c);
+	nodePtr create_end_node();
+	nodePtr start;
+	std::vector<Node> endNodes;
+	unsigned int i;
+};
+
+nodePtr Frag::create_end_node(){
+	Node end(true);
+	endNodes.push_back(end);
+	i++;
+	return &(endNodes[i-1]);
+}
+
+Frag::Frag(char c){
+	i = 0;
+	switch(c){
+	case '+':
+
+		break;
+	default: // a character
+		// Create the start node
+		start = new Node();
+		// Create the end node
+		nodePtr endPtr = create_end_node();
+		// Create an arrow between the two, using the character
+		start->create_arrow(endPtr, c, false);
+		break;
+	}
+}
+
+//
+int main(){
+	// Create a stack of fragments
+	std::stack<Frag> fragments;
+	// Read a character
+	char c = 'a';
+	// Create a fragment from the character
+	Frag tmp_frag(c);
+	// Push the fragment onto the stack
+	fragments.push(tmp_frag);
+	// read another character
+	c = 'b';
+	Frag tmp_frag(c);
+	fragments.push(tmp_frag);
+	// read another character
+
+}
+
+
 
 #endif
   
