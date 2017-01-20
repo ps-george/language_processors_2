4c5d14f4554ed402dcd72c810af71056f74034e3
George Punter <punter.s.g@gmail.com>
2017-01-20 00:10:15 +0000

    re to postfix seems to be working now, need to test more though

--- 1-regexes/c/bin/engine_3.cpp
+++ 1-regexes/c/bin/engine_3.cpp
@@ -31,6 +31,8 @@ nodePtr construct_nfa(char c) {
   return n;
 }
 
+string convert_to_postfix(string re);
+
 // First create the infix to postfix function
 /*
 class Token {
@@ -44,100 +46,91 @@ class Character : public Token {
 */
 
 
-map<char, int> operators {
-	{'\\',0},
-	{'(',1},
-	{')',2},
-	{'+',3},
-	{'.',4},
-	{'|',5}
-};
+int main(int argc, char *argv[]) {
+  string tmp;
+  string postfix;
+  while (getline(cin, tmp)) {
+    postfix = convert_to_postfix(tmp);
+  }
 
-// If o1 has higher or equal precedance
-bool lower_precedence(char o1, char o2){
-	if (operators[o1]>operators[o2]) return true;	 
-	return false;
+  return 0;
 }
 
 string convert_to_postfix(string re) {
   // Create the stacks
   stack<char> opStack;
-	vector<char> outQueue;
-	int count = 0;
+  vector<char> outQueue;
+  stack<pair<int, int>> counter;
+  int num_char = 0;
+  int num_alt = 0;
   // For each character in the string
   for (int i = 0; i < re.length(); i++) {
-		char c = re[i];
-		cout << c << endl;
-		switch(c){
-			default:
-				// If the token is a character, then push it to the output queue.
-				// If there is more than one of these, put a .
-				if (count>1){
-					count--;
-					outQueue.push_back('.');
-				}
-				outQueue.push_back(c);
-				count++;
-				break;
-			case '(': // If the token is a left parenthesis (i.e. "("), then push it onto the stack.
-				opStack.push('(');
-				break;
-			case ')':
-			/* Until the token at the top of the stack is a left parenthesis, pop operators off the stack onto the output queue.
-					Pop the left parenthesis from the stack, but not onto the output queue.
-					If the stack runs out without finding a left parenthesis, then there are mismatched parentheses. */
-					while (opStack.top()!='(') {
-						outQueue.push_back(opStack.top());
-						opStack.pop();
-					}
-					opStack.pop();
-				break;
-			case '|':
-			case '+':
-				if (opStack.empty()){
-					outQueue.push_back(c);
-				}
-				else if (!lower_precedence(c,opStack.top())){
-					opStack.push(c);  // If the token is an operator, if it has higher or equal precedence than the operator at the top of the opStack, push it onto the opStack
-				}
-				else {
-					while(lower_precedence(c,opStack.top())){ 
-					// If it has lower precedence than the operator at the top of the opStack pop all higher precedence operators from the opStack onto the output and push the operator onto the opStack
-						outQueue.push_back(opStack.top());
-						opStack.pop();
-					}
-				}
-				break;
-		}
-		cout << "outQueue:";
-		stack<char> clone = opStack;
-		for (int j = 0; j<outQueue.size();j++){
-			cout << outQueue[j];
-		}
-		
+    char c = re[i];
+    cout << c << endl;
+    switch (c) {
+    default:
+      // If the token is a character, then push it to the output queue.
+      // If there is more than one of these, put a .
+      if (num_char > 1) {
+        num_char--;
+        outQueue.push_back('.');
+      }
+      outQueue.push_back(c);
+      num_char++;
+      break;
+    case '(': // If there have been two characters before, concatenate them
+		// If the token is a left parenthesis (i.e. "("), raise the level of num_alt and num_char and reset them
+							
+      if (num_char > 1) {
+        num_char--;
+        outQueue.push_back('.');
+      }
+      counter.push(pair<int, int>(num_alt, num_char));
+      num_alt = 0;
+      num_char = 0;
+      break;
+    case ')':
+			// Dump all binary operators that are waiting to be added to output
+      while (num_char > 1) {
+        num_char--;
+        outQueue.push_back('.');
+      }
+      while (num_alt > 0) {
+        num_alt--;
+        outQueue.push_back('|');
+      }
+			// Retreive previous values of num_alt and num_char before open brackets
+      num_alt = counter.top().first;
+      num_char = counter.top().second;
+      counter.pop();
+      num_char++;
+      break;
+			
+    case '+': // For unary operators, just place them straight to the output
+      outQueue.push_back(c);
+      break;
+    case '|':
+      while (num_char > 1) {
+        num_char--;
+        outQueue.push_back('.');
+      }
+      num_alt++;
+      break;
+    }
   }
-	while (!opStack.empty()){
-		outQueue.push_back(opStack.top());
-		opStack.pop();
-	}
-	while (count > 1) {
-		outQueue.push_back('.');
-		count--;
-	}
-	std::string postfix(outQueue.begin(),outQueue.end());
-	cout << endl;
-	cout << postfix << endl;
-  return postfix;
-}
-
-int main(int argc, char *argv[]) { 
-	string tmp;
-	string postfix;
-	while (getline(cin,tmp)){
-		postfix = convert_to_postfix(tmp);
-	}
 	
-	return 0; 
-
-
+	// exhaust all remaining binary operators from the virtual stack
+  while ((num_char) > 0) {
+    outQueue.push_back('.');
+		num_char--;
+  }
+  while (num_alt > 0) {
+    outQueue.push_back('|');
+		num_alt--;
+  }
+	
+	// tranform outQueue vector into string
+  std::string postfix(outQueue.begin(), outQueue.end());
+  cout << postfix << endl;
+  return postfix;
 }
