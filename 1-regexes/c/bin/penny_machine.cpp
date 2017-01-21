85eea267f2460a6562fabbfd4875b6c405b3d553
George Punter <punter.s.g@gmail.com>
2017-01-21 17:21:37 +0000

    Moved implementation of penny machine functions into header file because can't figure out how to get the makefile working

--- 1-regexes/c/bin/penny_machine.cpp
+++ 1-regexes/c/bin/penny_machine.cpp
@@ -230,44 +230,4 @@ void Machine::reset(){
   // Clear all pennys
   pennys.clear();
   //
-  
-  
-}
-
-int main(){
-  string re;
-  string postfix;
-  string in;
-  int total = 0;
-  // Get the regular expression
-  getline(cin,re);
-  // Convert it to postfix
-  postfix = re2postfix(re);
-  cerr << postfix << endl;
-  // Construct the machine
-  
-  Machine m(postfix);
-  // Each time there's a new line
-  int matches = 0;
-  while (getline(cin, in)) {
-    // If there is no input, stop and print out statistics
-    if (!in[0]) break;
-    
-    total = 0;
-    m.start();
-    for (int i = 0; i<in.length(); i++){
-      matches = 0;
-      // cout << i+1 << "th character: " << in[i] << endl;
-      matches = m.input_char(in[i]);
-      total += matches;
-      // cout << "Matches: " << matches << " Total Matches thus far: " << total << endl;
-    }
-    // last character must be a match
-    if (matches>0) cout << "Match" << endl;
-    else cout << "NoMatch" << endl;
-    cerr << "Total matches: " << total << endl;
-    cerr << "Total loops: " << m.get_loopcount() << endl;
-  }
-  
-  cerr << "Total loops: " << m.get_loopcount() << endl;
 }
