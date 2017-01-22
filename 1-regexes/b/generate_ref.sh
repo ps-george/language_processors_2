a7919891e3ea142d7f18ace46f69a05f5dca7995
George Punter <punter.s.g@gmail.com>
2017-01-22 21:34:10 +0000

    Fixed generate_ref.sh, bored of writing tester for b since it is pointless

--- 1-regexes/b/generate_ref.sh
+++ 1-regexes/b/generate_ref.sh
@@ -4,11 +4,10 @@ mkdir -p test/ref
 for i in test/in/*.txt; do
   echo "Input file : ${i}"
   exec 6< $i
-  read -a sedpattern <&6
-  echo ${sedpattern[0]} ${sedpattern[1]};
+  read -r sedpattern <&6
      
   BASENAME=$(basename $i .txt);
-  echo "First_dt10_v3.pdf" | ./regex_substitute_ref.sh ${sedpattern[0]} ${sedpattern[1]} > test/ref/$BASENAME.txt
+  tail --lines=+2 $i | eval ./regex_substitute_ref.sh ${sedpattern}
 
   exec 6<&- 
 done
