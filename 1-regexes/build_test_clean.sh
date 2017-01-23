a40b517ae6710077e947029083e06e6451d6623a
George Punter <gsp14@eews304-013.ee.ic.ac.uk>
2017-01-23 17:45:28 +0000

    Adding automated test and build files

---
+++ 1-regexes/build_test_clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run test script for part a
+
+# Clean a
+make -C a clean
+
+# Run test script for part b
+
+# Clean b
+make -C b clean
+
+# Run test script for part c
+
+# Clean c
+make -C c clean
