adba40cc46ede4bf692e01b4e5078d6f9576a40b
George Punter <punter.s.g@gmail.com>
2017-02-03 17:36:33 +0000

    Removed shrink from diff_expr

--- 2-parsers/src/diff_expr.cpp
+++ 2-parsers/src/diff_expr.cpp
@@ -15,25 +15,25 @@ int main(int argc, char *argv[])
       variables.push_back(var);
     }
     
-    const Expression *diff1=parseAST();
+    const Expression *diff=parseAST();
     
     for (unsigned i = 0; i < variables.size(); ++i){
-      diff1 = diff1->differentiate(variables[i]);
+      diff = diff->differentiate(variables[i]);
       
     }
-    //diff1->print();
+    //diff->print();
     //fprintf(stdout, "\n"); // or fflush()
-    
+    /*
     int c = 1;
     while (c){
       c = 0;
       Expression::reset_changed();
-      diff1 = diff1->shrink();
-      c = diff1->has_changed();
+      diff = diff->shrink();
+      c = diff->has_changed();
       fprintf(stderr, "Changes: %d\n", c);
     }
-    
-    diff1->print();
+    */
+    diff->print();
     
     fprintf(stdout, "\n");
     return 0;
