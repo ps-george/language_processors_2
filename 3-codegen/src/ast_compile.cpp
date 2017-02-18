f0efe296dd5cae96d1bd548df179a06735ed9592
George Punter <punter.s.g@gmail.com>
2017-02-18 14:41:58 +0000

    Passing 5/10 compiler tests.

--- 3-codegen/src/ast_compile.cpp
+++ 3-codegen/src/ast_compile.cpp
@@ -34,10 +34,56 @@ void CompileRec(
         }
     
     // TODO : handle the others
-        
-    }else{
-        throw std::runtime_error("Unknown construct '"+program->type+"'");
-    }
+  }else if(program->type=="Input"){
+    std::cout << "input " << destReg << std::endl;
+  
+  }else if(program->type=="Add"){
+    std::string left = makeName("left");
+    std::string right = makeName("right");
+    // Compile the left
+    CompileRec(left, program->branches.at(0));
+    // Compile the right
+    CompileRec(right, program->branches.at(1));
+    
+    std::cout << "add " << destReg << " " << left << " " << right << std::endl;
+  
+  }else if(program->type=="Assign"){
+    // Create a zero register
+    std::string zero = makeName("zero");
+    std::cout<<"const "<<zero<<" 0"<<std::endl;
+    
+    // Assign the value of the assign branch to destReg
+    CompileRec(destReg, program->branches.at(0));
+    // Put the value in destReg into the id name
+    std::cout << "add " << program->value << " " << destReg << " " << zero << std::endl;
+  
+  }else if(program->type=="While"){
+    std::string zero = makeName("zero");
+    std::cout<<"const "<<zero<<" 0"<<std::endl;
+    
+    std::string start = makeName("start");
+    std::string end = makeName("end");
+    std::string c = makeName("condition");
+    // Get register value for the condition
+    // Set a label at the beginning of the
+    //  loop
+    std::cout << ":" << start << std::endl;
+    // Compile the condition into destination register c
+    CompileRec(c, program->branches.at(0));
+    
+    // At the beginning of the loop, check whether the
+    // value in the condition register is equal to zero
+    // if it is, go to the end
+    std::cout << "beq " << c << " " << zero << " " << end << std::endl;
+    // else do the rest of the code
+    CompileRec(destReg, program->branches.at(1));
+    // jump to the top again
+    std::cout << "beq " << zero << " " << zero << " " << start << std::endl;
+    // the end label
+    std::cout << ":" << end << std::endl;
+  }else{
+      throw std::runtime_error("Unknown construct '"+program->type+"'");
+  }
 }
 
 void Compile(
