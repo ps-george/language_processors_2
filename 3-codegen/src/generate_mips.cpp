e31a47e594d1f09e4ff82d179cfe59b8d1907233
George Punter <punter.s.g@gmail.com>
2017-02-22 20:05:39 +0000

    Created input/output structure for mips generate

---
+++ 3-codegen/src/generate_mips.cpp
@@ -0,0 +1,40 @@
+#include "ast.hpp"
+
+#include <cstdlib>
+// #include <iostream>
+#include <fstream>
+
+int main(int argc, char *argv[])
+{
+    std::ofstream out;
+    std::ifstream in;
+    
+    if (argc < 3){
+      std::cerr << "Not enough arguments provided." << std::endl;
+      return 1;
+    }
+    else if (argc == 3){
+      in.open(argv[1]);
+      if (!in.is_open()){
+        std::cerr << "Couldn't open input file." << std::endl;
+        return 1;
+      }
+      out.open(argv[2]);
+      if (!in.is_open()){
+        std::cerr << "Couldn't open output file." << std::endl;
+        return 1;
+      }
+    }
+    else {
+      std::cerr << "Too many arguments provided." << std::endl;
+      return 1;
+    }
+    TreePtr src=Parse(in);
+    
+    // Generate mips code instead of pretty print
+    
+    
+    PrettyPrint(out, src);
+    
+    return 0;
+}
